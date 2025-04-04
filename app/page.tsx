48edcafbecbb379aa26ce3dc1382387f58bf8513
Jameesh Moidunny <jameesh.moidunny@endeavourtechnology.com>
2025-04-04 10:03:29 +0100

    Test commit

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,14 @@ export default function Home() {
       const resolvedUser = await user;
       if (resolvedUser?.role === "EMPLOYEE") {
         redirect(`/employee/${resolvedUser.id}`);
-      } else {
-        redirect("/admin/dashboard");
+      } else if (resolvedUser?.role === "SUPERADMIN") {
+        redirect(`/superadmin/users`);
+      } else if (resolvedUser?.role === "ADMIN") {
+        redirect(`/admin/users`);
+      } else if (resolvedUser?.role === "MANAGER") {
+        redirect(`/manager/projects`);
       }
-    }
+    };
     setHomeUrl();
   }, [user]);
 
